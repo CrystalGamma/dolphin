d5c8344093990c951f756c58619ca786cea799d5
CrystalGamma <dolphin@crystalgamma.de>
2019-01-03 16:44:36 +0100

    add register cache for PPC64/ELFv2

---
+++ Source/Core/Core/PowerPC/JitTiered/PPC64RegCache.cpp
@@ -0,0 +1,308 @@
+// Copyright 2018 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#include "Core/PowerPC/JitTiered/PPC64RegCache.h"
+
+#include "Common/Assert.h"
+#include "Core/PowerPC/JitTiered/PPC64Baseline.h"
+
+namespace PPC64RegCache
+{
+static constexpr s16 GPROffset(u32 i)
+{
+  return s16(offsetof(PowerPC::PowerPCState, gpr) + 4 * i);
+}
+
+void RegisterCache::EstablishStackFrame(PPC64BaselineCompiler* comp)
+{
+  ASSERT(saved_regs == 0);
+  // allocate stack frame, save caller registers
+  comp->MFSPR(PPCEmitter::R0, PPCEmitter::SPR_LR);
+  comp->STD(PPCEmitter::R0, PPCEmitter::R1, 16);
+  saved_regs = 3;
+  comp->relocations.push_back(
+      comp->B(PPCEmitter::BR_LINK, comp->offsets.save_gprs + (18 - saved_regs) * 4));
+  comp->STD(PPCEmitter::R1, PPCEmitter::R1, -32 - 8 * saved_regs, PPCEmitter::UPDATE);
+  // copy our variables
+  comp->MoveReg(PPCEmitter::R31, PPCEmitter::R5);
+  reg_state[5] = FREE;
+  reg_state[31] = PPCSTATE_PTR;
+  comp->ADDI(PPCEmitter::R30, PPCEmitter::R6, 0x4000);
+  reg_state[5] = FREE;
+  reg_state[30] = TOC_PTR;
+}
+
+void RegisterCache::SetCR0(PPCEmitter* emit, GPR host_gpr)
+{
+  if (HoldsGuestRegister(host_gpr) && !IsSignExtended(host_gpr))
+  {
+    emit->EXTSW(host_gpr, host_gpr);
+    BindGPR(host_gpr, SEXT_R + GetGuestRegister(host_gpr));
+  }
+  // FIXME: implement SO emulation?
+  emit->STD(host_gpr, GetGPR(emit, PPCSTATE_PTR), s16(offsetof(PowerPC::PowerPCState, cr_val)));
+}
+
+GPR RegisterCache::GetArgumentRegister(u8 index)
+{
+  ASSERT(index < 10 && reg_state[index + 3] == ABI);
+  reg_state[index + 3] = SCRATCH;
+  return static_cast<GPR>(index + 3);
+}
+
+GPR RegisterCache::GetReturnRegister(u8 index)
+{
+  ASSERT(index < 10 && reg_state[index + 3] == ABI);
+  reg_state[index + 3] = SCRATCH;
+  return static_cast<GPR>(index + 3);
+}
+
+void RegisterCache::BindGPR(GPR host_gpr, u16 specifier)
+{
+  // can only bind to guest registers
+  ASSERT((specifier & 96) != 0);
+  u8 reg = specifier & 31;
+  // catch (some) invalidated registers
+  ASSERT(reg_state[host_gpr] == SCRATCH ||
+         (HoldsGuestRegister(host_gpr) && GetGuestRegister(host_gpr) == reg));
+  // invalidate any other values for the same guest register
+  for (u16& rs : reg_state)
+  {
+    if ((rs & 96) != 0 && (rs & 31) == reg)
+    {
+      rs = FREE;
+    }
+  }
+  reg_state[host_gpr] = specifier | FLAG_GUEST_UNSAVED | FLAG_IN_USE;
+}
+
+void RegisterCache::InvalidateAllRegisters()
+{
+  for (u8 i = 0; i < 32; ++i)
+  {
+    const u16 prev_state = reg_state[i];
+    ASSERT(!(prev_state & FLAG_GUEST_UNSAVED));
+    if (reg_state[i] != RESERVED && reg_state[i] != HOST_UNSAVED && reg_state[i] != TOC_PTR &&
+        reg_state[i] != PPCSTATE_PTR)
+    {
+      reg_state[i] = FREE;
+    }
+  }
+}
+
+GPR RegisterCache::GetGPR(PPCEmitter* emit, u16 specifier)
+{
+  u8 want_state = specifier & 96;
+  if (want_state == 0)
+  {
+    // search for GPR
+    u8 reg = specifier & 31;
+    bool found_vacancy = false;
+    bool found_clean = false;
+    u8 spot = 0;
+    for (u8 i = 0; i < 32; ++i)
+    {
+      const u16 state = reg_state[i];
+      if ((state & 31) == reg)
+      {
+        GPR res = static_cast<GPR>(i);
+        if (want_state != DIRTY_R && want_state != (state & 96))
+        {
+          if (want_state == ZEXT_R)
+          {
+            emit->RLDICL(res, res, 0, 32);
+          }
+          else
+          {
+            ASSERT(want_state == SEXT_R);
+            emit->EXTSW(res, res);
+          }
+        }
+        reg_state[i] |= (specifier & FLAG_GUEST_UNSAVED) | FLAG_IN_USE;
+        return res;
+      }
+      if (!found_vacancy)
+      {
+        if (state == FREE)
+        {
+          found_vacancy = true;
+          spot = i;
+        }
+        else if (!found_clean && (state & 96) != 0 && !(state & FLAG_GUEST_UNSAVED))
+        {
+          found_clean = true;
+          spot = i;
+        }
+      }
+    }
+    // ppcState pointer is not a guest register, so this should not recurse endlessly
+    const GPR ppcs = GetGPR(emit, PPCSTATE_PTR);
+    if (found_vacancy || found_clean)
+    {
+      const GPR res = static_cast<GPR>(spot);
+      emit->LWZ(res, ppcs, GPROffset(reg));
+      if (want_state == SEXT_R)
+      {
+        emit->EXTSW(res, res);
+      }
+      reg_state[spot] = specifier | FLAG_IN_USE;
+      return res;
+    }
+    // evict guest register
+    for (u8 i = 0; i < 32; ++i)
+    {
+      const GPR host_reg = static_cast<GPR>(i);
+      if (HoldsGuestRegister(host_reg) && !(reg_state[host_reg] & FLAG_IN_USE))
+      {
+        emit->STW(host_reg, ppcs, GPROffset(GetGuestRegister(host_reg)));
+        emit->LWZ(host_reg, ppcs, GPROffset(reg));
+        if (want_state == SEXT_R)
+        {
+          emit->EXTSW(host_reg, host_reg);
+        }
+        reg_state[host_reg] = specifier | FLAG_IN_USE;
+        return host_reg;
+      }
+    }
+    ERROR_LOG(DYNA_REC, "No free register to load guest register");
+    ASSERT(false);
+    return PPCEmitter::R0;
+  }
+  else if (specifier == SCRATCH || specifier == LOCKED)
+  {
+    const u8 start_search = specifier == SCRATCH ? 2 : 14;
+    const u8 end_search = specifier == SCRATCH ? 13 : 32;
+    bool found_vacancy = false;
+    bool found_clean = false;
+    u8 spot = 0;
+    for (u8 i = start_search; i < end_search; ++i)
+    {
+      const u16 state = reg_state[i];
+      if (state == FREE || state == ABI_FREE)
+      {
+        found_vacancy = true;
+        spot = i;
+        break;
+      }
+      else if (!found_clean && (state & 96) != 0 && !(state & FLAG_GUEST_UNSAVED))
+      {
+        found_clean = true;
+        spot = i;
+      }
+    }
+    // ppcState pointer is not a guest register, so this should not recurse endlessly
+    const GPR ppcs = GetGPR(emit, PPCSTATE_PTR);
+    if (found_vacancy || found_clean)
+    {
+      reg_state[spot] = specifier;
+      return static_cast<GPR>(spot);
+    }
+    // evict guest register
+    for (u8 i = start_search; i < end_search; ++i)
+    {
+      const GPR host_reg = static_cast<GPR>(i);
+      if (HoldsGuestRegister(host_reg) && !(reg_state[host_reg] & FLAG_IN_USE))
+      {
+        emit->STW(host_reg, ppcs, GPROffset(GetGuestRegister(host_reg)));
+        reg_state[host_reg] = specifier;
+        return host_reg;
+      }
+    }
+    ERROR_LOG(DYNA_REC, "No free register to allocate scratch register");
+    ASSERT(false);
+    return PPCEmitter::R0;
+  }
+  else
+  {
+    for (u8 i = 0; i < 32; ++i)
+    {
+      if (reg_state[i] == specifier)
+      {
+        return static_cast<GPR>(i);
+      }
+    }
+    ERROR_LOG(DYNA_REC, "No register for specifier %u was found", u32(specifier));
+    ASSERT(false);
+    return PPCEmitter::R0;
+  }
+}
+
+void RegisterCache::FlushHostRegister(PPCEmitter* emit, GPR host_gpr)
+{
+  const GPR ppcs = GetGPR(emit, PPCSTATE_PTR);
+  ASSERT(HoldsGuestRegister(host_gpr));
+  if (reg_state[host_gpr] & FLAG_GUEST_UNSAVED)
+  {
+    emit->STW(host_gpr, ppcs, GPROffset(GetGuestRegister(host_gpr)));
+    reg_state[host_gpr] &= ~FLAG_GUEST_UNSAVED;
+  }
+}
+
+void RegisterCache::FlushAllRegisters(PPCEmitter* emit)
+{
+  for (u8 i = 0; i < 32; ++i)
+  {
+    const GPR reg = static_cast<GPR>(i);
+    if (HoldsGuestRegister(reg))
+    {
+      FlushHostRegister(emit, reg);
+    }
+  }
+}
+
+GPR RegisterCache::PrepareCall(PPCEmitter* emit, u8 num_parameters)
+{
+  ASSERT(num_parameters <= 10);
+  for (u8 i = 3; i < 3 + num_parameters; ++i)
+  {
+    const GPR reg = static_cast<GPR>(i);
+    if (HoldsGuestRegister(reg))
+    {
+      FlushHostRegister(emit, reg);
+    }
+    reg_state[i] = ABI;
+  }
+  for (u8 i = 3 + num_parameters; i < 13; ++i)
+  {
+    const GPR reg = static_cast<GPR>(i);
+    if (HoldsGuestRegister(reg))
+    {
+      FlushHostRegister(emit, reg);
+    }
+    reg_state[i] = ABI_FREE;
+  }
+  return PPCEmitter::R12;
+}
+
+void RegisterCache::BindCall(PPCEmitter* emit)
+{
+  emit->MTSPR(PPCEmitter::SPR_CTR, PPCEmitter::R12);
+}
+
+void RegisterCache::PerformCall(PPCEmitter* emit, u8 num_return_gprs)
+{
+  ASSERT(num_return_gprs < 10);
+  emit->BCCTR();
+  for (u8 i = 3; i < 3 + num_return_gprs; ++i)
+  {
+    ASSERT(reg_state[i] == SCRATCH || reg_state[i] == ABI_FREE);
+    reg_state[i] = ABI;
+  }
+  for (u8 i = 3 + num_return_gprs; i < 13; ++i)
+  {
+    ASSERT(reg_state[i] == SCRATCH || reg_state[i] == ABI_FREE);
+    reg_state[i] = FREE;
+  }
+}
+
+void RegisterCache::PrepareReturn(PPCEmitter* emit, u8 num_return_gprs)
+{
+  FlushAllRegisters(emit);
+  for (u8 i = 3; i < 3 + num_return_gprs; ++i)
+  {
+    reg_state[i] = ABI;
+  }
+}
+
+}  // namespace PPC64RegCache
---
+++ Source/Core/Core/PowerPC/JitTiered/PPC64RegCache.h
@@ -0,0 +1,122 @@
+// Copyright 2019 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "Common/PPCEmitter.h"
+#include "Core/PowerPC/PowerPC.h"
+
+class PPC64BaselineCompiler;
+
+namespace PPC64RegCache
+{
+using GPR = PPCEmitter::GPR;
+/// the least significant 5 bit specify which guest register (if applicable) or what other state the
+/// register is in. the next two bits decide what state the guest register is in (0b00 ↦ not a guest
+/// register, 0b01 ↦ dirty, 0b10 ↦ zero extended, 0b11 ↦ sign extended) upper bits are flags
+enum : u16
+{
+  FREE = 0,
+  HOST_UNSAVED = 1,
+  RESERVED = 2,
+  TOC_PTR = 3,
+  PPCSTATE_PTR = 4,
+  SCRATCH = 5,
+  /// some non-guest-register value, is in non-volatile host register and is not invalidated
+  LOCKED = 6,
+  /// argument for a prepared (but not performed) call, or return value of a recently-performed call
+  /// not available for register caching, but can be claimed using GetArgumentRegister and
+  /// GetReturnRegister
+  ABI = 7,
+  /// volatile register during call preparation. available for scratch allocation, but not guest
+  /// registers
+  ABI_FREE = 8,
+  DIRTY_R = 32,
+  ZEXT_R = 64,
+  SEXT_R = 96,
+  /// means the register has not been written to ppcState yet.
+  /// passing this to GetGPR signals that the guest register will be written there without fail
+  FLAG_GUEST_UNSAVED = 128,
+  /// this is used to avoid overwriting registers that are used by the current intruction
+  FLAG_IN_USE = 256,
+};
+struct RegisterCache
+{
+  /// state of host registers at this point
+  std::array<u16, 32> reg_state;
+  u8 saved_regs = 0;
+
+  RegisterCache()
+  {
+    // register 0 is special in addi and load/store, so better not use it for now
+    reg_state[0] = RESERVED;
+    // register 1 is always the stack pointer
+    reg_state[1] = RESERVED;
+    // register 2 is supposed to be the TOC pointer, but it's just a volatile register AFAICS
+    reg_state[2] = FREE;
+    // register 3 is the first argument, starts out as the JIT pointer, which we don't use
+    reg_state[3] = FREE;
+    // register 4 is the second argument, starts out as the offset value from the block DB (not used
+    // currently)
+    reg_state[4] = FREE;
+    // register 5 is the third argument, starts out as the ppcState pointer
+    reg_state[5] = PPCSTATE_PTR;
+    // register 6 is the fourth argument, starts out as the ToC pointer
+    reg_state[6] = TOC_PTR;
+    // remaining volatile registers
+    for (u8 i = 7; i < 13; ++i)
+    {
+      reg_state[i] = FREE;
+    }
+    // register 13 is the thread pointer per ABI
+    reg_state[13] = RESERVED;
+    // registers 14–31 are non-volatile, so start out unsaved
+    for (u8 i = 14; i < 32; ++i)
+    {
+      reg_state[i] = HOST_UNSAVED;
+    }
+  }
+  bool HoldsGuestRegister(GPR host_gpr) { return (reg_state[host_gpr] & 96) != 0; }
+  bool IsSignExtended(GPR host_gpr) { return (reg_state[host_gpr] & 96) == SEXT_R; }
+  bool IsZeroExtended(GPR host_gpr) { return (reg_state[host_gpr] & 96) == ZEXT_R; }
+  GPR GetGuestRegister(GPR host_gpr) { return static_cast<GPR>(reg_state[host_gpr] & 31); }
+  /// invalidates all register references that are not LOCKED, TOC_PTR or PPCSTATE_PTR
+  void ReleaseRegisters()
+  {
+    for (u8 i = 0; i < 32; ++i)
+    {
+      if (reg_state[i] != LOCKED)
+      {
+        reg_state[i] &= ~FLAG_IN_USE;
+      }
+      if (reg_state[i] == SCRATCH)
+      {
+        reg_state[i] = FREE;
+      }
+    }
+  }
+  void FreeGPR(GPR host_gpr) { reg_state[host_gpr] = FREE; }
+  GPR GetArgumentRegister(u8 index);
+  GPR GetReturnRegister(u8 index);
+  void BindGPR(GPR host_gpr, u16 specifier);
+  void InvalidateAllRegisters();
+
+  void EstablishStackFrame(PPC64BaselineCompiler* comp);
+  void FlushHostRegister(PPCEmitter* emit, GPR gpr);
+  void FlushAllRegisters(PPCEmitter* emit);
+
+  GPR GetGPR(PPCEmitter* emit, u16 specifier);
+
+  /// invalidates all non-LOCKED register references
+  GPR PrepareCall(PPCEmitter* emit, u8 num_parameters);
+  void BindCall(PPCEmitter* emit);
+  /// invalidates all register references that are not LOCKED, TOC_PTR or PPCSTATE_PTR
+  void PerformCall(PPCEmitter* emit, u8 num_return_gprs);
+  /// invalidates all register references that are not LOCKED, TOC_PTR or PPCSTATE_PTR
+  void PrepareReturn(PPCEmitter* emit, u8 num_return_values);
+
+  void SetCR0(PPCEmitter* emit, GPR host_gpr);
+};
+
+}  // namespace PPC64RegCache
