2e71863d36d5b30c5396963c9421e2201abe100a
CrystalGamma <dolphin@crystalgamma.de>
2019-01-03 17:36:41 +0100

    regcache fix

--- Source/Core/Core/PowerPC/JitTiered/PPC64RegCache.cpp
+++ Source/Core/Core/PowerPC/JitTiered/PPC64RegCache.cpp
@@ -128,10 +128,12 @@ GPR RegisterCache::GetGPR(PPCEmitter* emit, u16 specifier)
         {
           found_vacancy = true;
           spot = i;
-        } else if (!found_clean && (state & 96) != 0 && !(state & (FLAG_GUEST_UNSAVED | FLAG_IN_USE)) {
+        }
+        else if (!found_clean && (state & 96) != 0 && !(state & (FLAG_GUEST_UNSAVED | FLAG_IN_USE)))
+        {
           found_clean = true;
           spot = i;
-				}
+        }
       }
     }
     // ppcState pointer is not a guest register, so this should not recurse endlessly
